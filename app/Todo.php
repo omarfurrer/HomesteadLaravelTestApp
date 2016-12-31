fa6d733b5fefb4f5082f102baf40f9449168f8f0
Omar Ruedy <theh2h@gmail.com>
2016-12-31 22:51:19 +0200

    COMMIT EVERYTHING

---
+++ app/Todo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'todos';
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+}
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,3 @@
+@foreach($todos as $todo)
+{{ $todo->name }}
+@endforeach
